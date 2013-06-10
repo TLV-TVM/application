9f6f1980c67a9e4fad2dee87fdfe39fe0aba5e45
TLV TVM <tlvtvm@comet-studio.com>
2013-06-10 16:21:03 +0200

    index ajout apple meta

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
 	
 	<meta name="viewport" content="height=device-height,width=device-width,initial-scale=1.0,maximum-scale=1.0, user-scalable=no"  >
 	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<link rel="apple-touch-startup-image" href="img/jqt_startup.png" />
 	<link rel="apple-touch-icon" href="img/jqtouch.png" />
 	<link rel="apple-touch-icon" sizes="72x72" href="img/jqtouch.png" /><!-- 72x72-->
 	<link rel="apple-touch-icon" sizes="114x114" href="img/jqtouch.png" /><!-- 114x144-->
 	
 	
-	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery-1.9.1.min.js"></script>
 	
 	<!-- jQuery UI Map v3 -->
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"></script>
